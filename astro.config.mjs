cd8ecdb0e5e13645b77791f43e4d75f88389c674
qwerty0zero <igoricloud98@gmail.com>
2026-09-16 01:01:00 +0200

    chore: init project with Astar 7, setup linters, formater, and pre-commits

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  output: 'static',
+  compressHTML: true,
+
+  build: {
+    inlineStylesheets: 'always',
+  },
+
+  vite: {
+    build: {
+      minify: true,
+      cssCodeSplit: false,
+    },
+  },
+});
